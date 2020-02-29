6e326badf1df659b4ad8a33701434b54d5a7f4d0
braxton-mclean <braxton.mclean5@gmail.com>
2020-02-29 15:56:45 -0500

    Made a visual representation of Unit turns taking place using GameTick logic
    Migrated Unit System logic to Unit Component. Made Unit Sprite and Unit Stat components data onlt (will likely add set/get methods to stat values though).
    On initialization, the GameTick system connects signals of subsystems so when signals are emitted by subsystems or the GameTick system, all relevant calls are completed. Even though the option to associate signals is available in the editor, doing so via code is a good way for us to single place to keep track of the majority of signal connects.
    GameTick operates by emitting signals related to the phase that is started/finished, including the gametick itself. Units currently have one signal to process, started_gt_phase, and is associated with incrementing its tick_counter with its current speed value. This method is no longer necessary for MapGrid (as Godot hierarchy and tree work makes more sense to keep this and readied_units to the Units object rather than the MapGrid)

--- src/scripts/ColiseumGametickSystem.gd
+++ src/scripts/ColiseumGametickSystem.gd
@@ -8,9 +8,11 @@ signal started_gt_phase
 signal finished_gt_phase
 signal started_action_phase
 signal finished_action_phase
+signal finished_gametick
 
-onready var intro_node = get_node("Intro")
+onready var units_node = get_node("Units")
 
+var start_gametick = false
 var gametick_counter = 0
 
 
@@ -19,35 +21,58 @@ func _ready():
 	# Initialize specific instance data that cannot wait for onready
 	
 	# Wait for level intro to be completed
-	yield(intro_node.play_intro(), "finished_intro")
+	#intro_node.animation_player.play_intro()
+	#yield(intro_node.animation_player, "finished_intro")
 	
-	# Begin gametick
-	_gametick_loop()
+	# Connect components to relevant signals
+	for unit in units_node.units:
+		self.connect("started_gt_phase", unit, "progress_tick_counter")
+		unit.connect("completed_turn", units_node, "unready_unit")
+		unit.connect("unit_ready", units_node, "ready_unit")
 
 
 func _gametick_loop():
 	# Battle Phase Loop
 	while true:
-		# Status Phase
+		self.gametick_counter += 1
+		emit_signal("started_gametick", self.gametick_counter)
+		# ===== Status Phase =====
 		emit_signal("started_status_phase")
 		emit_signal("finished_status_phase")
 		
-		# Charge Phase / Held Action
+		# ===== Charge / Held Action Phase =====
 		emit_signal("started_charge_phase")
 		emit_signal("finished_charge_phase")
 		
-		# GameTick+ Phase
-		self.gametick_counter += 1
+		# ===== GameTick+ Phase =====
 		emit_signal("started_gt_phase", self.gametick_counter)
-		print("Gametick Loop Iteration: " + self.gametick_counter)
+		print("Gametick Loop Iteration: " + str(self.gametick_counter))
 		emit_signal("finished_gt_phase")
 		
-		# Action Phase
+		# ===== Action Phase =====
 		emit_signal("started_action_phase")
-		yield(get_tree().create_timer(2.0), "timeout")
+		
+		# Dummy logic just to see processing Unit.
+		# This logic should be moved out of GameTick and into Units
+		for unit in self.units_node.readied_units:
+			var unit_stats = unit.get_node("UnitStatComponent")
+			print(unit.name + "'s Turn!")
+			unit.take_turn()
+			yield(unit, "completed_turn")
+		
 		emit_signal("finished_action_phase")
+		
+		# ===== Finished GameTick =====
+		emit_signal("finished_gametick")
+
+
+func _input(event):
+	if not start_gametick:
+		if Input.is_key_pressed(KEY_SPACE):
+			start_gametick = true
+			_gametick_loop()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+#	pass
--- src/scripts/UnitStatComponent.gd
+++ src/scripts/UnitStatComponent.gd
@@ -1,21 +1,20 @@
 extends Node2D
 
+signal unit_ready
+
 export var stat_strength = 1
 export var stat_vitality = 1
-
 export var stat_charisma = 1
 export var stat_confidence = 1
-
-export var stat_speed = 5
 export var stat_dexterity = 1
-
+export var stat_speed = 5
 export var stat_move = 4
 
 var tick_counter = 0
 
 
 func _ready():
-	_unit_update_loop()
+	pass
 
 
 func _unit_update_loop():
@@ -27,11 +26,8 @@ func _unit_update_loop():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if (self.tick_counter >= 50):
-		print(self.name + " - TickCounter: " + str(self.tick_counter))
-		self.tick_counter -= int(randf() * 50)
-
-
-func progress_tick_counter(gametick_counter):
-	self.tick_counter += self.stat_speed
+#func _process(delta):
+#	pass
+#	if (self.tick_counter >= 50):
+#		print(self.name + " - TickCounter: " + str(self.tick_counter))
+#		self.tick_counter -= int(randf() * 50)
