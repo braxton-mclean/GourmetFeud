53f3da4e78cd525aaf1f6371b944eb53d76dad2d
Faizon Williams <faizon@protonmail.com>
2020-04-02 22:27:17 -0400

    edited music on title screen and view settings

--- src/menus/TitleScreen.gd
+++ src/menus/TitleScreen.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 
+
 func _ready():
 	$MarginContainer/VBoxContainer/VBoxContainer2/PressStartToPlay.grab_focus()
+	OS.window_maximized = true
 #Selects start play button on start of scene
 func _physics_process(delta):
 	if $MarginContainer/VBoxContainer/VBoxContainer2/PressStartToPlay.is_hovered() ==true:
