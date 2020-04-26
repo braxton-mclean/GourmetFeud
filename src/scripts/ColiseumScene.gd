56147d15013284cfd3d7e7555bb1b953eeeff620
LeftPhalange <ethanbovard@hotmail.com>
2020-04-26 15:54:45 -0400

    Added support for traversables with the MapGridComponent

--- src/scripts/ColiseumScene.gd
+++ src/scripts/ColiseumScene.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 const mapGrid = preload("res://src/scripts/MapGridComponent.gd")
 # Declare member variables here. Examples:
@@ -8,9 +8,9 @@ const mapGrid = preload("res://src/scripts/MapGridComponent.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var inst = mapGrid.new().generateMapGridComponent()
+	print ("Ready called on Colosseum A")
+	var inst = mapGrid.new("TraversablesColosseumA").mapGrid
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- src/scripts/MapGridComponent.gd
+++ src/scripts/MapGridComponent.gd
@@ -2,18 +2,46 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 
 var activeUnit = []
+var mapGrid = []
 
-func _ready():
-	generateMapGridComponent()
-	
-func generateMapGridComponent ():
-	var dimensions = [5, 5] # dimensions defined by width and height respectively in the list
+#func _ready():
+#	pass
+
+func _init (traversableName):
+	var traversable = File.new()
+	if traversable.open("res://src/scenes/Traversables/" + traversableName + ".csv", File.READ) != 1: # this file can be read
+		traversable = traversable.get_as_text()
+		# print ("Traversable data: " + traversable)
+		var lines = traversable.split('\n', false, 0)
+		var columns = lines[0].split(',', false, 0)
+		var heightLength = len(lines)
+		var widthLength = len(columns)
+		if heightLength > 0 and widthLength > 0:
+			print ("Height: " + str(heightLength) + ", width: " + str(widthLength))
+			generateMapGridComponent (traversable, [widthLength, heightLength])
+		else:
+			print ("Please check your traversables file, it may be empty.")
+	else:
+		print ("Traversable not found. Please check your scenes/Traversables directory.")
+
+func parseCSV (value):
+	if value == "TRUE":
+		return 1 # 0 meaning, is traversable
+	elif value == "FALSE":
+		return 0 # 1 meaning, is not traversable
+		
+
+func generateMapGridComponent (mapGridCSV, mapGridSize):
+	mapGridCSV = mapGridCSV.split('\n', false, 0)
+	var dimensions = mapGridSize # dimensions defined by width and height respectively in the list
 	var arr = [] # entire array, elements in the list will be treated as rows
 	for _height in range(0, dimensions[1]): # dimensions[1] is the height
 		var row = []
+		var line = mapGridCSV[_height].split(',', false, 0)
 		for _width in range(0, dimensions[0]): # how many elements can go in one row?
-			row.append(0)
+			row.append(parseCSV(line[_width]))
 		arr.append(row)
+	print (arr)
 
 func insertAtPosition (x, y, val, arr):
 	arr[x][y] = val
