eeea6532c3730c78d0ad08d4339e3f3f855738af
Braxton McLean <braxton.mclean5@gmail.com>
2020-04-30 07:40:07 -0400

    Revert "Revert "Merge branch 'master' into feature-done-game""
    This reverts commit fad4cb5e633dd1205ccea93ef307a4449291f132.

--- src/combat/units/stats/CharacterStats.gd
+++ src/combat/units/stats/CharacterStats.gd
@@ -6,16 +6,17 @@ signal health_changed(prev_health, new_health)
 signal health_depleted()
 
 var modifiers = {}
+var strength_attributes = []
+var speed_attributes = []
 
 var max_health : int setget set_max_health
 var health : int
-var strenth_attributes = []
-var strength : int 
+var strength : int setget , get_strength
 var vitality : int
 var charisma : int
 var confidence : int
 var dexterity : int
-var speed : int
+var speed : int setget , get_speed
 var move : int
 var remaining_actions : int
 var remaining_moves : int
@@ -23,7 +24,6 @@ var default_moves : int
 var default_actions : int
 
 var tick_counter = 0
-
 
 func initialize(stats : StartingStats):
 	self.max_health = stats.max_health
@@ -40,10 +40,20 @@ func initialize(stats : StartingStats):
 	self.remaining_moves = default_moves
 	self.remaining_actions = default_actions
 
+func get_strength ():
+	return strength
+
+func get_speed ():
+	return speed
 
 func set_max_health(value):
 	max_health = max(0, value)
 
+func add_strength_attr(value):
+	self.strength_attributes.append(value)
+
+func add_speed_attr(value):
+	self.speed_attributes.append(value)
 
 func take_damage(hit):
 	var prev_health = self.health
@@ -52,7 +62,6 @@ func take_damage(hit):
 	emit_signal("health_changed", prev_health, health)
 	if self.health == 0:
 		emit_signal("health_depleted")
-
 
 func heal(amount):
 	var prev_health = self.health
