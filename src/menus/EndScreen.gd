4b00c5367d9c52d665113cdd65e075994b3f1210
Faizon Williams <faizon@protonmail.com>
2020-04-30 05:08:40 -0400

    Added Functionality to end screen

--- src/menus/EndScreen.gd
+++ src/menus/EndScreen.gd
@@ -1,19 +1,13 @@
-extends Node2D
+
+extends Control
 
 
 func _ready():
-	$MarginContainer/VBoxContainer/VBoxContainer2/MainMenu.grab_focus()
-	OS.window_maximized = true
-#Selects start play button on start of scene
-func _physics_process(delta):
-	if $MarginContainer/VBoxContainer/VBoxContainer2/MainMenu.is_hovered() ==true:
-		$MarginContainer/VBoxContainer/VBoxContainer2/MainMenu.grab_focus()
-#Adds hover and click functionality to button
-	if $MarginContainer/VBoxContainer/VBoxContainer2/EndGame.is_hovered() ==true:
-		$MarginContainer/VBoxContainer/VBoxContainer2/EndGame.grab_focus()
-		
-func _on_MainMenu_pressed():
-	get_tree().change_scene("res://src/menus/TitleScreen.tscn")
-#Load scene on click
-func _on_EndGame_pressed():
-	get_tree().quit()
+	$MarginContainer/Menu/Buttons/MainMenu.grab_focus()
+	for button in $MarginContainer/Menu/Buttons.get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+
+
+func _on_Button_pressed(scene_to_load):
+	print(scene_to_load)
+	get_tree().change_scene(scene_to_load)
