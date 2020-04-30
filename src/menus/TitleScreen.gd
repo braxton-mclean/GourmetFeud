6006629fba82d71050bae3461043d7688412a356
Faizon Williams <faizon@protonmail.com>
2020-04-30 04:46:16 -0400

    Refactored Buttons, added dynamic scene transition

--- src/menus/TitleScreen.gd
+++ src/menus/TitleScreen.gd
@@ -1,20 +1,13 @@
-extends Node2D
 
+extends Control
 
 
 func _ready():
-	$MarginContainer/VBoxContainer/VBoxContainer2/PressStartToPlay.grab_focus()
-	OS.window_maximized = true
-#Selects start play button on start of scene
-func _physics_process(delta):
-	if $MarginContainer/VBoxContainer/VBoxContainer2/PressStartToPlay.is_hovered() ==true:
-		$MarginContainer/VBoxContainer/VBoxContainer2/PressStartToPlay.grab_focus()
-#Adds hover and click functionality to button
-	if $MarginContainer/VBoxContainer/VBoxContainer2/Exit.is_hovered() ==true:
-		$MarginContainer/VBoxContainer/VBoxContainer2/Exit.grab_focus()
-		
-func _on_PressStartToPlay_pressed():
-	get_tree().quit()
-#Load scene on click
-func _on_Exit_pressed():
-	get_tree().quit()
+	$MarginContainer/Menu/Buttons/PlayButton.grab_focus()
+	for button in $MarginContainer/Menu/Buttons.get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+
+
+func _on_Button_pressed(scene_to_load):
+	print(scene_to_load)
+	get_tree().change_scene(scene_to_load)
